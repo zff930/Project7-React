2f708ca3583106460582c981f053b934e7e9d263
Feifei Zhang <zwz108@gmail.com>
2025-10-26 11:04:28 -0700

    Added post request and get all posts request.

---
+++ backend/routers/post.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const router = express.Router();
+
+const auth = require("../middleware/auth");
+const multer = require('../middleware/multer-config');
+
+const postCtrl = require("../controllers/post");
+
+// For every request coming to this endpoint, use the corresponding function on our post controller.
+// Add auth middleware and multer before controller.
+router.post("/", auth, multer, postCtrl.createPost);
+router.get('/', auth, postCtrl.getAllPosts);
+// router.get('/:id', auth, postCtrl.getPostById);
+
+// router.put("/:id", auth, multer, saucesCtrl.updateOneSauce);
+// router.delete("/:id", auth, saucesCtrl.deleteOneSauce);
+// router.post('/:id/like', auth, saucesCtrl.setLike);
+
+module.exports = router;
