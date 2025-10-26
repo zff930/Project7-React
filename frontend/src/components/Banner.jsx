facc31be5a71f9b6a0883394711403f7b6db17f6
Feifei Zhang <zwz108@gmail.com>
2025-10-26 11:12:04 -0700

    Fixed issue of logo not displayed by correcting import content.

---
+++ frontend/src/components/Banner.jsx
@@ -0,0 +1,19 @@
+import logo from "../assets/icon.png";
+import "../styles/Banner.css";
+
+function Banner() {
+  const title = "React Project";
+
+  return (
+    <div>
+      <div className="rp-banner">
+        <div className="rp-banner-row">
+          <img src={logo} alt="React Project" className="rp-logo" />
+          <h1 className="rp-title">{title}</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Banner; 
