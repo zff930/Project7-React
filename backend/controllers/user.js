230b33c6ef2db381fb9c2b12beb605c2a82d065e
Feifei Zhang <zwz108@gmail.com>
2025-10-28 13:58:37 -0700

    Generated the token when signing up so that if it will be redirected to homepage after signed up.

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -2,76 +2,72 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const { User } = require("../models");
 
-exports.signup = (req, res, next) => {
-  const { firstName, lastName, email, password } = req.body;
-
-  bcrypt
-    .hash(password, 10)
-    .then((hashedPassword) => {
-      return User.create({
-        firstName,
-        lastName,
-        email,
-        password: hashedPassword,
-      });
-    })
-    .then((user) => {
-      res.status(201).json({
-        message: "User created successfully!",
-        user: {
-          id: user.userId,
-          firstName: user.firstName,
-          lastName: user.lastName,
-          email: user.email,
-        },
-      });
-    })
-    .catch((err) => {
-      console.error("Signup error:", err);
-      res.status(500).json({
-        error: "Error creating user!",
-      });
-    });
+// Generate JWT helper
+const generateToken = (user) => {
+  return jwt.sign(
+    { userId: user.id, email: user.email },
+    process.env.JWT_SECRET || "RANDOM_TOKEN_SECRET",
+    { expiresIn: "24h" }
+  );
 };
 
-exports.login = (req, res, next) => {
-  const { email, password } = req.body;
-  let foundUser;
+exports.signup = async (req, res, next) => {
+  try {
+    const { firstName, lastName, email, password } = req.body;
 
-  User.findOne({ where: { email } })
-    .then((user) => {
-      if (!user) {
-        return res.status(401).json({
-          error: "User not found!",
-        });
-      }
-      foundUser = user;
-      return bcrypt.compare(password, user.password);
-    })
-    .then((isValid) => {
-      if (!isValid) {
-        return res.status(401).json({
-          error: "Invalid password!",
-        });
-      }
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-      const token = jwt.sign(
-        { userId: foundUser.userId, email: foundUser.email },
-        process.env.JWT_SECRET || "RANDOM_TOKEN_SECRET", // provide a fallback if JWT_SECRET is missing in .env
-        { expiresIn: "24h" }
-      );
-
-      res.status(200).json({
-        message: "User logged in successfully!",
-        token: token,
-        userId: foundUser.userId,
-        email: foundUser.email,
-      });
-    })
-    .catch((err) => {
-      console.error("Login error:", err);
-      res.status(500).json({
-        error: "Internal Server Error!",
-      });
+    const user = await User.create({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
     });
+
+    const token = generateToken(user);
+    res.status(201).json({
+      message: "User created successfully!",
+      token,
+      userId: user.id,
+      email: user.email,
+    });
+  } catch (err) {
+    console.error("Signup error:", err);
+    res.status(500).json({
+      error: "Error creating user!",
+    });
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+    if (!user) {
+      return res.status(401).json({
+        error: "User not found!",
+      });
+    }
+
+    const isValid = await bcrypt.compare(password, user.password);
+    if (!isValid) {
+      return res.status(401).json({
+        error: "Invalid password!",
+      });
+    }
+
+    const token = generateToken(user);
+    res.status(200).json({
+      message: "User logged in successfully!",
+      token: token,
+      userId: user.id,
+      email: user.email,
+    });
+  } catch (err) {
+    console.error("Login error:", err);
+    res.status(500).json({
+      error: "Internal Server Error!",
+    });
+  }
 };
