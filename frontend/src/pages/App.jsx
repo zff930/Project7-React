464cf18459199177dfd99366ad2d4850b4bfcd17
Feifei Zhang <zwz108@gmail.com>
2025-10-28 14:05:20 -0700

    Protected homepage and profile page.

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,16 +1,19 @@
 import "../styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from './HomePage';
-import SignUp from './SignUpPage';
-import LogIn from './LogInPage';
+import ProtectedRoute from '../components/ProtectedRoute';
+import Home from "./HomePage";
+import SignUp from "./SignUpPage";
+import LogIn from "./LogInPage";
+import Profile from "./ProfilePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<ProtectedRoute> <Home /> </ProtectedRoute>}/>
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
+        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
       </Routes>
     </Router>
   );
