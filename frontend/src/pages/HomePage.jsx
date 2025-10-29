7c3969a44debce59cb62d8d6381c27d6c6b9016c
Feifei Zhang <zwz108@gmail.com>
2025-10-28 21:24:09 -0700

    Added Banner component to the homepage.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 // display all posts, desc by timestamp, PostForm
 // if user already read by display text for "if read"
 
-// import Banner from "../components/Banner";
+import Banner from "../components/Banner";
 import "../styles/HomePage.css";
 
 function Home() {
   return (
     <>
-      {/* <Banner /> */}
+      <Banner />
       <div className="home-page-shoutout">I'm a home page, yay!</div>
     </>
   );
