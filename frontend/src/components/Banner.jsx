6704020ca7db68529977beefcaf5e8de2d6aced0
Feifei Zhang <zwz108@gmail.com>
2025-10-28 21:23:25 -0700

    Added logo, title, and buttons to the homepage.

--- frontend/src/components/Banner.jsx
+++ frontend/src/components/Banner.jsx
@@ -1,19 +1,32 @@
 import logo from "../assets/icon.png";
 import "../styles/Banner.css";
+import { Link } from "react-router-dom";
 
 function Banner() {
-  const title = "React Project";
+  const title = "GroupomaniaConnect";
 
   return (
     <div>
-      <div className="rp-banner">
-        <div className="rp-banner-row">
-          <img src={logo} alt="React Project" className="rp-logo" />
-          <h1 className="rp-title">{title}</h1>
+      <div className="banner">
+        <div className="banner-row">
+          <div className="logo-title">
+            <img src={logo} alt="React Project" className="logo" />
+            <h1 className="title">{title}</h1>
+          </div>
+
+          <div className="buttons">
+            <Link to="/signup">
+              <button>Sign Up</button>
+            </Link>
+
+            <Link to="/login">
+              <button>Log In</button>
+            </Link>
+          </div>
         </div>
       </div>
     </div>
   );
 }
 
-export default Banner; 
+export default Banner;
