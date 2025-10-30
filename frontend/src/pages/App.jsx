e2c9763baa058d744563f69636fa4ab066ecad48
Feifei Zhang <zwz108@gmail.com>
2025-10-29 21:11:47 -0700

    Added protected PostPage.

--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,19 +1,35 @@
 import "../styles/App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ProtectedRoute from '../components/ProtectedRoute';
+import ProtectedRoute from "../components/ProtectedRoute";
 import Home from "./HomePage";
 import SignUp from "./SignUpPage";
 import LogIn from "./LogInPage";
+import PostPage from "./PostPage";
 import Profile from "./ProfilePage";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<ProtectedRoute> <Home /> </ProtectedRoute>}/>
+        <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<LogIn />} />
-        <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
+        <Route
+          path="/post/:id"
+          element={
+            <ProtectedRoute>
+              <PostPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <ProtectedRoute>
+              <Profile />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </Router>
   );
