fceff4d4d352fe9a0602348d47789ca4be477627
Feifei Zhang <zwz108@gmail.com>
2025-10-29 21:27:44 -0700

    Converted .then().catch() to try...catch.. and async...await...

--- backend/controllers/post.js
+++ backend/controllers/post.js
@@ -1,32 +1,38 @@
 const { User, Post } = require("../models");
 
-exports.createPost = (req, res, next) => {
-  const url = req.protocol + "://" + req.get("host");
+exports.createPost = async (req, res, next) => {
+  try {
+    const url = req.protocol + "://" + req.get("host");
 
-  Post.create({
-    userId: req.auth.userId,
-    content: req.body.content,
-    imageUrl: req.file ? `${url}/images/${req.file.filename}` : null,
-    likesCount: 0,
-    commentsCount: 0,
-  })
-    .then((post) => res.status(201).json({ message: "Post created successfully!", post }))
-    .catch((err) => res.status(400).json({ error: err }));
+    const post = await Post.create({
+      userId: req.auth.userId,
+      content: req.body.content,
+      imageUrl: req.file ? `${url}/images/${req.file.filename}` : null,
+      likesCount: 0,
+      commentsCount: 0,
+    });
+
+    res.status(201).json({ message: "Post created successfully!", post });
+  } catch (err) {
+    res.status(400).json({ error: err });
+  }
 };
 
 // GET all posts
-exports.getAllPosts = (req, res, next) => {
-  Post.findAll({
-    include: {
-      model: User,
-      as: 'author',
-      attributes: ['id', 'firstName', 'lastName', 'email'],
-    },
-    order: [['createdAt', 'DESC']],
-  })
-    .then((posts) => res.status(200).json(posts))
-    .catch((err) => {
-      console.error(err);
-      res.status(500).json({ error: 'Failed to retrieve posts' });
+exports.getAllPosts = async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        as: "author",
+        attributes: ["id", "firstName", "lastName", "email"],
+      },
+      order: [["createdAt", "DESC"]],
     });
+    
+    res.status(200).json(posts);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Failed to retrieve posts" });
+  }
 };
