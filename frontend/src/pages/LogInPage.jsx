885351df0f7e282a289bbdbd4e9fa41ed371b2cd
Feifei Zhang <zwz108@gmail.com>
2025-11-12 14:03:26 -0800

    Set up the api base url to avoid repetition.

--- frontend/src/pages/LogInPage.jsx
+++ frontend/src/pages/LogInPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../config";
 
 function LogIn() {
   const navigate = useNavigate(); // call hook
@@ -13,7 +14,7 @@ function LogIn() {
     e.preventDefault();
 
     try {
-      const response = await fetch("http://localhost:3000/api/auth/login", {
+      const response = await fetch(`${API_BASE_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(credentials),
