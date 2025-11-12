38ea00f1c544d9dd58976a6a96898b254a4c50b9
Feifei Zhang <zwz108@gmail.com>
2025-11-12 14:10:03 -0800

    Set up the api base url to avoid repetition.

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../config";
 
 function SignUp() {
   const navigate = useNavigate(); // call hook
@@ -18,7 +19,7 @@ function SignUp() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await fetch("http://localhost:3000/api/auth/signup", {
+      const response = await fetch(`${API_BASE_URL}/auth/signup`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
