bdd42b9790084451fb5ea07fcf7678259d9d1ca5
Feifei Zhang <zwz108@gmail.com>
2025-11-14 11:16:16 -0800

    Created backend for protected route.

---
+++ backend/routers/protected.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const auth = require("../middleware/auth");
+
+router.get('/', auth, (req, res) => {
+  res.status(200).json({ message: "OK" });
+});
+
+module.exports = router;
