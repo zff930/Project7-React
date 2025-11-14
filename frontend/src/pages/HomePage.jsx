3822496ed46368a841dc1b195e6500db7b5eec13
Feifei Zhang <zwz108@gmail.com>
2025-11-14 15:31:31 -0800

    Imported api base url.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,6 +2,7 @@
 
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { API_BASE_URL } from "../config";
 import PostForm from "../components/PostForm";
 import Banner from "../components/Banner";
 import "../styles/HomePage.css";
