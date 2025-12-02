c9e041e0f6e174710b4cf7c748c00255be8a77db
Feifei Zhang <zwz108@gmail.com>
2025-12-02 13:52:55 -0800

    Added comments.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'; // Recommended but optional for newer React versions
+import ReactDOM from 'react-dom/client'; // Give access to the newer React 18 root API (createRoot) which replaces the older ReactDOM.render method
 import './styles/index.css';
 import App from './pages/App';
 
+// React app will mount at <div id="root"></div> in index.html.
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // A wrapper for safer dev that activates extra checks and warnings
+  // for potential problems but gets automatically removed when app built.
   <React.StrictMode>
+    // Render React app inside root.
     <App />
   </React.StrictMode>
 );
