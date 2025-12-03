b7ad63c8853b847fb2ddcc8873c0286ff018f2f6
Feifei Zhang <zwz108@gmail.com>
2025-12-02 17:13:51 -0800

    Added comments.

--- backend/app.js
+++ backend/app.js
@@ -16,7 +16,8 @@ const app = express(); // Express app instance to define middleware, routes, and
 // ===============================
 app.use(cors); // Enable CORS for all routes
 app.use(express.json()); // Parse JSON request bodies -> req.body
-app.use("/api/uploads", express.static(path.join(__dirname, "uploads"))); // Serve static uploads
+app.use("/api/uploads", express.static(path.join(__dirname, "uploads"))); // __dirname = a Node.js global variable representing the current folder where 'uploads' folder lives
+                                                                          // Make 'uploads' folder publicly accessible
 
 // ===============================
 // Routes
