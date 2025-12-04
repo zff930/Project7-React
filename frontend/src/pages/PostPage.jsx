bd78b15a834a396d57fc02b33c2e1a9f03a34a56
Feifei Zhang <zwz108@gmail.com>
2025-12-04 02:52:15 -0800

    Removed import React.

--- frontend/src/pages/PostPage.jsx
+++ frontend/src/pages/PostPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Banner from "../components/Banner";
 import { API_BASE_URL } from "../config";
